ecbf3e09fa515f07df5239c916ec71754b4df8e9
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2022-04-28 12:30:27 +0200

    refactor(app): rm unused functions

--- src/LinearAlgebraicRepresentation.jl
+++ src/LinearAlgebraicRepresentation.jl
@@ -36,43 +36,6 @@ module LinearAlgebraicRepresentation
 
 
 	"""
-		Chain = SparseArrays.SparseVector{Int8,Int}
-
-	Alias declation of LAR-specific data structure.
-	Binary `SparseVector` to store the coordinates of a `chain` of `N-cells`. It is
-	`nnz=1` with `value=1` for the coordinates of an *elementary N-chain*, constituted by
-	a single *N-chain*.
-	"""
-	const Chain = SparseArrays.SparseVector{Int8,Int}
-
-
-	"""
-		ChainOp = SparseArrays.SparseMatrixCSC{Int8,Int}
-
-	Alias declation of LAR-specific data structure.
-	`SparseMatrix` in *Compressed Sparse Column* format, contains the coordinate
-	representation of an operator between linear spaces of `P-chains`.
-	Operators ``P-Boundary : P-Chain -> (P-1)-Chain``
-	and ``P-Coboundary : P-Chain -> (P+1)-Chain`` are typically stored as
-	`ChainOp` with elements in ``{-1,0,1}`` or in ``{0,1}``, for
-	*signed* and *unsigned* operators, respectively.
-	"""
-	const ChainOp = SparseArrays.SparseMatrixCSC{Int8,Int}
-
-
-	"""
-		ChainComplex = Array{ChainOp,1}
-
-	Alias declation of LAR-specific data structure. It is a
-	1-dimensional `Array` of `ChainOp` that provides storage for either the
-	*chain of boundaries* (from `D` to `0`) or the transposed *chain of coboundaries*
-	(from `0` to `D`), with `D` the dimension of the embedding space, which may be either
-	``R^2`` or ``R^3``.
-	"""
-	const ChainComplex = Array{ChainOp,1}
-
-
-	"""
 		LARmodel = Tuple{Points,Array{Cells,1}}
 
 	Alias declation of LAR-specific data structure.
